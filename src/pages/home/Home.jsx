83588173150a83e89494843471fe584622bdf2bc
crunchy89 <rocker.hunt@gmail.com>
2022-01-17 09:45:38 +0800

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -20,12 +20,12 @@ const Home = () => {
             <div className="d-sm-none card rounded-circle mx-auto m-0"  style={{overflow:'hidden',height:'200px',width:'200px'}}>
               <img src={me} alt="" className="w-100" />
             </div>
-            <p className="mt-2" align='justify'>Lahir pada 11 Mei 1989. Berpengalaman menggunakan JavaScript, Laravel, Codeigniter, MySQL, ReactJs. Dengan pengalaman lebih dari beberapa tahun di front-end dan back-end. Bergairah dalam mempelajari hal baru dan selalu mengikuti perkembangan teknologi terkini</p>
+            <p className="mt-2" align='justify'>Lahir pada 11 Mei 1989. Dengan Pengalaman lebih dari beberapa tahun di front-end dan back-end. Bergairah dalam mempelajari hal baru dan selalu mengikuti perkembangan teknologi terkini</p>
             <Link to="/tentang" className="btn btn-light m-1">Lihat Lebih Banyak</Link>
           </div>
     </Bg>
     <Mobile>
-    <p className="mt-2 text-light p-3" align='justify'>Lahir pada 11 Mei 1989. Berpengalaman menggunakan JavaScript, Laravel, Codeigniter, MySQL, ReactJs. Dengan pengalaman lebih dari beberapa tahun di front-end dan back-end. Bergairah dalam mempelajari hal baru dan selalu mengikuti perkembangan teknologi terkini</p>
+    <p className="mt-2 text-light p-3" align='justify'>Lahir pada 11 Mei 1989. Dengan Pengalaman lebih dari beberapa tahun di front-end dan back-end. Bergairah dalam mempelajari hal baru dan selalu mengikuti perkembangan teknologi terkini</p>
     <div className="d-flex w-100 justify-content-center">
             <Link to="/tentang" className="btn btn-light m-1">Lihat Lebih Banyak</Link>
     </div>
